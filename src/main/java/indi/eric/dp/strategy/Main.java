6d02db375f65e6652e433930d54254302a3870a5
sqin <sheng.qin@hotmail.com>
2018-08-17 23:36:36 +0800

    first commit for test

---
+++ src/main/java/indi/eric/dp/strategy/Main.java
@@ -0,0 +1,10 @@
+package indi.eric.dp.strategy;
+
+/**
+ * Created by qinsheng on 2018/8/17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("design pattern practice");
+    }
+}
